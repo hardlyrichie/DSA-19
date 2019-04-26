ce538c068f0a6a17683260b81a0f2ac31d412eb5
hardlyrichie <hardlyrichie@gmail.com>
2019-04-25 23:30:43 -0400

    Finish DP 01

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,47 @@
 public class DiceRollSum {
+    /*
+    * 5 DP Steps
+    * Subproblem:
+    * DP[i] = # of 6 sided dice roll sequences that sum up to i
+    * Guess:
+    * Guess which sequences of dice sum up to i
+    * Recurrence Relation:
+    * DP[i] = diceRoll(i-1) + diceRoll(i-2) + diceRoll(i-3) + ... + diceRoll(i-6)
+    * Recurse/Memoize:
+    * Recurse back to sum N
+    * Solve Original:
+    * return DP[n]
+    */
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(6n) = O(n)
+    // Space: O(N+1) = O(N)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        int[] DP = new int[N + 1];
+
+        // Store base case in memo
+        DP[0] = 1;
+
+        // Solve subproblems
+        return diceRollSumRecurs(N, DP);
+    }
+
+    private static int diceRollSumRecurs(int i, int[] DP) {
+        // Have we already solved this subproblem
+        if (DP[i] != 0) return DP[i];
+
+        // 6 possible subproblems as one dice can only be values 1 - 6
+        int j = i > 6 ? 6 : i;
+
+        // Solve subproblems
+        int answer = 0;
+        for (int k = 1; k <= j; k++) {
+            answer += diceRollSumRecurs(i - k, DP);
+        }
+
+        // store our answer and return it
+        DP[i] = answer;
+
+        return answer;
     }
 
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,51 @@
-public class LongestIncreasingSubsequence {
+import java.util.Arrays;
+import java.util.HashMap;
 
-    // Runtime: TODO
-    // Space: TODO
+public class LongestIncreasingSubsequence {
+    /*
+     * 5 DP Steps
+     * Subproblem:
+     * DP[i] = length of the longest subsequence starting at A[i]
+     * Guess:
+     * Guess which of the next subsequences is longest
+     * Recurrence Relation:
+     * DP[i] = DP[i+1] + 1, as long as the next value (i+1) is greater that the current value (i), sequence is increasing
+     * Recurse/Memoize:
+     * Recurse back to find length of DP[i]
+     * Solve Original:
+     * return max(DP)
+     */
+
+    // Runtime: O(n^2)
+    // Space: O(n)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        int[] DP = new int[A.length];
+
+        int answer = 0;
+        // Find which subsequence is longest (testing subsequences starting at i)
+        for (int i = 0; i < A.length; i++) {
+            answer = Math.max(recurse(A, DP, i), answer);
+        }
+
+        return answer;
+    }
+
+    // DP[i] is the length of the LIS whose first element is A[i]
+    private static int recurse(int[] A, int[] DP, int i) {
+        // Have we already solved this subproblem
+        if (DP[i] != 0) return DP[i];
+
+        int answer = 1;
+        for (int j = i + 1; j < A.length; j++) {
+            // checks whether next number in sequence is an increase. If so find that subsequence's length. Skips (remove) values less than
+            if (A[j] > A[i]) {
+                answer = Math.max(answer, recurse(A, DP, j) + 1);
+            }
+        }
+
+        // Store our answer and return it
+        DP[i] = answer;
+
+        return DP[i];
     }
 }
